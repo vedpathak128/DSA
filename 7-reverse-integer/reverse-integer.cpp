f8140cebf67888b2a70b20093b2eb2c289268b75
Ved Pathak <vedpathak128@gmail.com>
2026-08-11 20:02:01 +0530

    Time: 2 ms (38.27%) | Memory: 8.5 MB (84.34%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+//ved
+    int reverse(int x) {
+        long long int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            rev=rev*10 + rem;
+             x=x/10;
+        }
+
+        if(rev>pow(2,31)-1 || rev<pow(-2,31)){
+            return 0;
+         }
+         else{
+            return rev;
+         }
+    }
+};
